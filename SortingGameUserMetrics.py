6119e62537dbd8d993fe93386d007b359c344254
DonBusto <33113637+DonBusto@users.noreply.github.com>
2019-03-27 15:04:14 +0100

    atributos del csv solucionado

--- SortingGameUserMetrics.py
+++ SortingGameUserMetrics.py
@@ -2,9 +2,9 @@ import csv
 import json
 import sys
 
-#filename = sys.argv[1]
+filename = sys.argv[1]
 filename_local = "c:/users/usuario/downloads/sortingameusermetrics.json"
-with open(filename_local, "r") as myfile: #CAMBIAR OPCIONALMENTE filename POR filename_local Y ASIGNAR EL NOMBRE DEL ARCHIVO LOCAL A ESA VARIABLE
+with open(filename, "r") as myfile: #CAMBIAR OPCIONALMENTE filename POR filename_local Y ASIGNAR EL NOMBRE DEL ARCHIVO LOCAL A ESA VARIABLE
     data = myfile.read().replace('\n','')
     metrics_parsed = json.loads(data)
 
@@ -29,24 +29,41 @@ metrics_data_id = metrics_parsed
 csv_file = open("metrics.csv", "w", newline='')
 csvwriter = csv.writer(csv_file, delimiter=";")
 count = 0
+array_attr = []
+count_first = 0
+for metric in metrics_data_id:
+    if count_first == 0:
+        header = metric.keys()
+        for element in header:
+            array_attr.append(element)
+        count_first += 1
+    else:
+        header = metric.keys()
+        for element in header:
+            if element not in array_attr:
+                array_attr.append(element)
+        count_first += 1
+
+print(array_attr)
+
+
 
 for metric in metrics_data_id:
     if count==0:
-        header = metric.keys()
         # print(header)
         array = ['id','username','type','age','countryISO','allTimeLevelsPlayed','currentMaxLevel','currentTotalPoints','gender','latestLevelPlayed','municipalityName','latestLevelResult', 'levelStatistics']
-        csvwriter.writerow(array)
+        csvwriter.writerow(array_attr)
         count += 1
     else:
 
         try:
             array_valores = []
             # print(array.__len__())
-            for i in range(0, array.__len__()):
-                if array[i] not in metric:
+            for i in range(0, array_attr.__len__()):
+                if array_attr[i] not in metric:
                     array_valores.append('')
                 else:
-                    array_valores.append(metric[array[i]])
+                    array_valores.append(metric[array_attr[i]])
 
             csvwriter.writerow(array_valores)
 
