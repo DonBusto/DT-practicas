11345ab1bd540bc3c0b42af0246992ac368c74f6
DonBusto <33113637+DonBusto@users.noreply.github.com>
2019-03-27 16:29:04 +0100

    Programa cuestionario, csv y arreglo de bug

--- SortingGameUserQuestionnaire.py
+++ SortingGameUserQuestionnaire.py
@@ -3,27 +3,27 @@ import json
 import sys
 
 filename = sys.argv[1]
-filename_local = "c:/users/usuario/downloads/sortingameusermetrics.json"
+filename_local = "c:/users/usuario/downloads/sortinggameuserquestionnaire.json"
 with open(filename, "r") as myfile: #CAMBIAR OPCIONALMENTE filename POR filename_local Y ASIGNAR EL NOMBRE DEL ARCHIVO LOCAL A ESA VARIABLE
     data = myfile.read().replace('\n','')
-    metrics_parsed = json.loads(data)
+    questionnaire_parsed = json.loads(data)
 
-    print(metrics_parsed)
+    print(questionnaire_parsed)
 
-metrics_data_id = metrics_parsed
-csv_file = open("metrics.csv", "w", newline='')
+questionnaire_data_id = questionnaire_parsed
+csv_file = open("questionnaire.csv", "w", newline='')
 csvwriter = csv.writer(csv_file, delimiter=";")
 count = 0
 array_attr = []
 count_first = 0
-for metric in metrics_data_id:
+for question in questionnaire_data_id:
     if count_first == 0:
-        header = metric.keys()
+        header = question.keys()
         for element in header:
             array_attr.append(element)
         count_first += 1
     else:
-        header = metric.keys()
+        header = question.keys()
         for element in header:
             if element not in array_attr:
                 array_attr.append(element)
@@ -31,9 +31,7 @@ for metric in metrics_data_id:
 
 print(array_attr)
 
-
-
-for metric in metrics_data_id:
+for question in questionnaire_data_id:
     if count==0:
         # print(header)
         csvwriter.writerow(array_attr)
@@ -44,10 +42,10 @@ for metric in metrics_data_id:
             array_valores = []
             # print(array.__len__())
             for i in range(0, array_attr.__len__()):
-                if array_attr[i] not in metric:
+                if array_attr[i] not in question:
                     array_valores.append('')
                 else:
-                    array_valores.append(metric[array_attr[i]])
+                    array_valores.append(question[array_attr[i]])
 
             csvwriter.writerow(array_valores)
 
@@ -57,5 +55,3 @@ for metric in metrics_data_id:
 
 
 csv_file.close()
-
-#print(metrics_data_id)
